f45417f9b1234467e4b082a0c3f93893261f2c72
theNickola <kurilchik.nv@gmail.com>
2023-02-03 23:38:27 +0300

    Добавьте файлы проекта.

---
+++ CollectionManager/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CollectionManager.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
